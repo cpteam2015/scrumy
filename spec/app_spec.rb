be3410b4e9b4b1ed6aa17d91ad90206e7d67ecb2
xhitedev <e.greg.bx@gmail.com>
2015-10-18 15:43:35 +0200

    fixed dependencies

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,10 +2,8 @@ require 'spec_helper'
 
 describe 'server', type: feature, js: true do
   before :each do
-    visit '/'
   end
 
-  it 'has a backlog' do
-    #expect(page).to have('#backlog')
-  end
+  it 'has a backlog'
+  
 end
