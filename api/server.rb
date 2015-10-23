705a86611c32dd174e9406c307776ef96287b078
nla15 <misstynet@hotmail.fr>
2015-10-23 11:54:20 +0200

    US:8 Tache:4 Pourquoi:API modification d'une US * On a fait les changements suivants: *

--- api/server.rb
+++ api/server.rb
@@ -45,7 +45,7 @@ class ScrumyAPI < Sinatra::Base
     halt 200, connector.replace(request.body.read).to_json
   end
 
- delete '/user_stories/:id' do
+  delete '/user_stories/:id' do
     content_type :json
     connector = settings.us_connector
     halt 200, connector.delete(params[:id]).to_json
